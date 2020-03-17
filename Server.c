b1fbbc9c4c3ecce116fbad538e04667ea193b0d2
Sandeep Kumar Singh <44255731+itissandeep98@users.noreply.github.com>
2020-03-18 02:34:41 +0530

    Update and rename shm_Server.c to Server.c

--- Server.c
+++ Server.c
@@ -14,23 +14,48 @@ key_t key;
 int shmid ;
 
 char *clientdata;
-char prevClient[100]; //previous client name
+char prevClient[100]; 
 char** parsed;
 
+int ind=0;
+
 int clientCount = 0;
+pthread_mutex_t lock;
 
 pthread_t thread[1024];
 
 int cond=1;
 
-
+typedef struct
+{
+	char *msglist[20];
+} msg;
+volatile msg *arr;
 struct client{
 	char name[20];
 	int key;
 	int index;
 };
-struct client Client[1024];
 
+struct client Client[100];
+
+void enqueue(char *msg)
+{
+	ind = ind % 20;
+	arr->msglist[ind++] = msg;
+}
+
+void show()
+{
+	int j = 0;
+	printf("------------------------------------\n");
+	while (j < ind)
+	{
+		printf("%d: %s\n", j, arr->msglist[j]);
+		j++;
+	}
+	printf("------------------------------------\n");
+}
 void sigintHandler(int sig_num){
 	fflush(stdout);
 	printf("exiting\n");
@@ -40,7 +65,10 @@ void sigintHandler(int sig_num){
 	for(int i = 0 ; i < clientCount ; i ++)
 		pthread_join(thread[i],NULL);
 
+	pthread_mutex_destroy(&lock);
+
 	exit(0);
+
 }
 
 char **parse(char* input){
@@ -68,10 +96,6 @@ void send_to_client(int index,char * msg){
 	int shmid_receiver=shmget((key_t)Client[index].key+1,1024,0666|IPC_CREAT);
 	char * data_to_send=(char*) shmat(shmid_receiver,(void*)0,0);
 	strncpy(data_to_send,msg,100);
-
-	// shmdt(data_to_send);
-	// shmctl(shmid_receiver,IPC_RMID,NULL);
-
 }
 
 void * networking(void * ClientDetail){
@@ -89,25 +113,39 @@ void * networking(void * ClientDetail){
         if(strcmp(prevmsg,msg)==0){
             continue;
         }
+		pthread_mutex_lock(&lock);
+		enqueue(msg);
+		
+
 		fflush(stdout);
 	    printf(">>>Data read from Client %d: %s\n",clientDetail->index,msg);
         strncpy(prevmsg,msg,100);
 
-		char** decode_message=parse(msg);
 
 		char * temp=(char *) malloc(20 * sizeof(char));
-		if(strcmp(decode_message[0],"SEND")==0 || strcmp(decode_message[0],"send")==0){
+		if(memcmp(msg,"SEND",4)==0 || memcmp(msg,"send",4)==0){
+			char *start = &msg[5];
+  			char *end = &msg[7];
+			char *substr = (char *)calloc(1, end - start + 1);
+  			memcpy(substr, start, end - start);
+			sprintf(temp,"\t\t%s says %s\n",clientDetail->name, msg+7);
+			int index=atoi(substr);
+			if( index== clientDetail->index){
+				printf("Data can't be written to the same client\n");
 
-			strcpy(temp,decode_message[2]);
-			int index=atoi(decode_message[1]);
-			if(index<clientCount){
+			}
+			else if(index<clientCount){
 				send_to_client(index,temp);
 				fflush(stdout);
 				printf(">>>Data Written to client %d : %s\n",index,msg);
 			}
+			else{
+				printf("client doesn't exist\n");
+			}
 		}
-		else if(strcmp(decode_message[0],"ALL")==0||strcmp(decode_message[0],"all")==0){
-			strcpy(temp,decode_message[1]);
+
+		else if(memcmp(msg,"ALL",3)==0||memcmp(msg,"all",3)==0){
+			sprintf(temp,"\t\t%s\t: %s\n",clientDetail->name,msg+4);
 			fflush(stdout);
 			printf(">>>Data Sent to every client\n");
 			for(int i=0;i<clientCount;i++){
@@ -118,17 +156,39 @@ void * networking(void * ClientDetail){
 		}
 		else {
 			fflush(stdout);
-			printf("command Not found\n");
+			printf("command Not found: %s\n",msg);
 		}
-
-
+		// sleep(10);
+		pthread_mutex_unlock(&lock);
     }
 }
+
+void* handleclient(){
+	while (cond)
+	{
+		if (strcmp(prevClient, clientdata) == 0)
+		{
+			continue;
+		}
+		parsed = parse(clientdata);
+		fflush(stdout);
+		printf("Connected Client %d %s\n", clientCount, parsed[0]);
+		strncpy(prevClient, clientdata, 100);
+
+		strcpy(Client[clientCount].name, parsed[0]);
+		Client[clientCount].key = atoi(parsed[1]);
+		Client[clientCount].index = clientCount;
+		pthread_create(&thread[clientCount], NULL, networking, (void *)&Client[clientCount]);
+		clientCount++;
+	}
+}
+
 
 
 int main(){
 	fflush(stdout);
-	printf("server running\n");
+	printf("Server Running\n");
+
 	key = 1000;
 	shmid = shmget(key,1024,0666|IPC_CREAT);
 	clientdata = (char*) shmat(shmid,(void*)0,0);
@@ -136,26 +196,27 @@ int main(){
 	strncpy(prevClient,clientdata,100);
 	signal(SIGINT, sigintHandler);
 
+	key_t key3 = 1003;
+	int shmid3 = shmget(key3, 1024, 0666 | IPC_CREAT);
+	arr = shmat(shmid3, (void *)0, 0);
+
+	if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init has failed\n");
+        return 1;
+    }
+	pthread_t th;
+	pthread_create(&th, NULL, handleclient, NULL);
+
+	char *temp = (char *)malloc(sizeof(char) * 100);
 	while(1){
-		if(strcmp(prevClient,clientdata)==0){
-            continue;
-        }
-		parsed=parse(clientdata);
-		fflush(stdout);
-		printf("Connected Client %d %s\n",clientCount,parsed[0]);
-		strncpy(prevClient,clientdata,100);
-
-
-		strcpy(Client[clientCount].name,parsed[0]);
-		Client[clientCount].key = atoi(parsed[1]) ;
-		Client[clientCount].index = clientCount;
-		pthread_create(&thread[clientCount], NULL, networking,  (void *) &Client[clientCount]);
-		clientCount++;
+		fgets(temp, 100, stdin);
+		if (memcmp(temp, "show", 4) == 0 || memcmp(temp, "SHOW", 4) == 0)
+		{
+			show();
+		}
+		else if (memcmp(temp, "exit", 4) == 0 || memcmp(temp, "EXIT", 4) == 0){
+			sigintHandler(0);
+		}
 	}
-
-	shmdt(clientdata);
-	shmctl(shmid,IPC_RMID,NULL);
-	for(int i = 0 ; i < clientCount ; i ++)
-		pthread_join(thread[i],NULL);
-
 }
