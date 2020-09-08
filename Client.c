a819cdd0338ea8036292042aa8313520b01c7143
itissandeep98 <itissandeep98@gmail.com>
2020-09-09 01:39:32 +0530

    comments added

--- Client.c
+++ Client.c
@@ -2,68 +2,93 @@
 
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 9890
+#define CLIENT_FILES "client-files/"
 
 int socket_desc, file_size, file_desc;
 struct sockaddr_in server;
 char reply_msg[BUFSIZ], filename[BUFSIZ], *data;
 
-void sigintHandler(int sig_num)
+void sigintHandler(int sig_num) // function to handle smooth closing of the socket
 {
 	fflush(stdout);
 	printf("!!!!Closing socket!!!!\n");
+	char msg[] = "exit";
+	write(socket_desc, msg, strlen(msg)); // sending info to server that client is exiting
 	close(socket_desc);
 	exit(0);
 }
 
 int main(int argc, char **argv)
 {
+	// setting a system call on the keyboard interrupts so that the socket closes safely in any situation
 	signal(SIGINT, sigintHandler);
 
+	// creating a socket
 	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+	// setting the propery of reusability on the socket port to get rid of the bind error
 	int a = 1;
 	setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR, &a, sizeof(int));
+
 	if (socket_desc == -1)
 	{
 		perror("Could not create socket");
 		return 1;
 	}
 
-	server.sin_addr.s_addr = inet_addr(SERVER_IP);
-	server.sin_family = AF_INET;
-	server.sin_port = htons(SERVER_PORT);
+	// setting socket properties
+	server.sin_family = AF_INET;				   // to use ipv4 mechanism
+	server.sin_addr.s_addr = inet_addr(SERVER_IP); // to accepts client from any ip address
+	server.sin_port = htons(SERVER_PORT);		   // assigning port
+
+	printf("\nSocket Created\n");
 
 	if (connect(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
 	{
 		perror("Connection failed");
 		return 1;
 	}
+
+	printf("\nSocket Connected\n");
+
 	while (1)
 	{
+		// taking file name from the user
 		printf("\nPlease enter file name to search: ");
 		scanf("%s", filename);
+
+		// if the user wishes to exit
 		if (strcmp(filename, "exit") == 0 || strcmp(filename, "quit") == 0 || strcmp(filename, "q") == 0)
 		{
 			printf("\n!!!!Exiting!!!!!\n");
-			char msg[] = "exit";
-			write(socket_desc, msg, strlen(msg));
-			close(socket_desc);
+			sigintHandler(0);
 			break;
 		}
+		// sending file name to the server
 		write(socket_desc, filename, strlen(filename));
+
+		// receiving the response from server whether the file exists or not
 		recv(socket_desc, reply_msg, 2, 0);
 
 		if (strcmp(reply_msg, "OK") == 0)
 		{
+			// receiving the file size of the requested file
 			recv(socket_desc, &file_size, sizeof(int), 0);
+
+			// making the file to get stored inside the clients direcotry only
 			data = malloc(file_size);
 			char location[BUFSIZ];
-			strcpy(location, "client-files/");
+			strcpy(location, CLIENT_FILES);
 			strcat(location, filename);
 			file_desc = open(location, O_CREAT | O_EXCL | O_WRONLY, 0666);
+
+			// receiving data inside the file
 			recv(socket_desc, data, file_size, 0);
+
+			// writing the data to the above created file
 			write(file_desc, data, file_size);
 			close(file_desc);
-			printf("\nFile Received\n");
+			printf("\nFile Received of size %d\n", file_size);
 		}
 		else
 		{
--- Server.c
+++ Server.c
@@ -1,11 +1,11 @@
 #include "headers.h"
 
 #define SERVER_PORT 9890
-#define IP_ADDRESS "127.0.0.1" // localhost
+#define SERVER_FILES "server-files/"
 
 int socket_desc, socket_client, *new_sock, c = sizeof(struct sockaddr_in);
 
-void sigintHandler(int sig_num)
+void sigintHandler(int sig_num) // function to handle smooth closing of the socket
 {
 	fflush(stdout);
 	printf("!!!!Closing socket!!!!\n");
@@ -20,32 +20,38 @@ int HandleConn(void *socket_desc)
 	struct stat obj;
 	int file_desc, file_size;
 
+	// Recieve File name from the connected client
 	recv(socket, file_name, BUFSIZ, 0);
-	if (strcmp(file_name, "exit") == 0 || strcmp(file_name, "") == 0)
+
+	if (strcmp(file_name, "exit") == 0 || strcmp(file_name, "") == 0) // compare the data recieved from client if it wants to exit
 	{
-		printf("\nClient Disconnected\n\n!!Exiting!!\n");
-		sigintHandler(0);
+		printf("\nClient Disconnected\n");
 		return 0;
 	}
 
+	// since server files are located inside its directory so concatinating that location with the file name
 	char location[BUFSIZ];
-	strcpy(location, "server-files/");
+	strcpy(location, SERVER_FILES);
 	strcat(location, file_name);
 
-	strcpy(file_name,"");
+	strcpy(file_name, "");
 
 	FILE *fp = fopen(location, "r");
-	if (fp != NULL)
+	if (fp != NULL) // checking if the file exists or not
 	{
 		printf("\nFile Found: %s\n", location);
 
+		// sending a response to client that file exists on server
 		strcpy(server_response, "OK");
 		write(socket, server_response, strlen(server_response));
 
+		// Obtaining the file size and sending to client to excepect this size of file
 		stat(location, &obj);
 		file_desc = open(location, O_RDONLY);
 		file_size = obj.st_size;
 		send(socket, &file_size, sizeof(int), 0);
+
+		// Sending the file
 		sendfile(socket, file_desc, NULL, file_size);
 
 		printf("\nFile Sent\n ");
@@ -55,8 +61,10 @@ int HandleConn(void *socket_desc)
 	}
 	else
 	{
-		perror("\nfile Not found ");
+		// Showing error if the file was not found
+		perror("\nFile Not found\n");
 		strcpy(server_response, "NO");
+		// sending a NO response to the client
 		write(socket, server_response, strlen(server_response));
 		return 1;
 	}
@@ -64,11 +72,14 @@ int HandleConn(void *socket_desc)
 
 int main()
 {
+	// setting a system call on the keyboard interrupts so that the socket closes safely in any situation
 	signal(SIGINT, sigintHandler);
 
 	struct sockaddr_in server, client;
-
+	// creating a socket
 	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	
+	// setting the propery of reusability on the socket port to get rid of the bind error
 	int a = 1;
 	setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR, &a, sizeof(int));
 
@@ -78,26 +89,28 @@ int main()
 		return 1;
 	}
 
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons(SERVER_PORT);
+	// setting socket properties
+	server.sin_family = AF_INET;		  // to use ipv4 mechanism
+	server.sin_addr.s_addr = INADDR_ANY;  // to accepts client from any ip address
+	server.sin_port = htons(SERVER_PORT); // assigning port
 
-	if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
+	if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) // binding the socket
 	{
 		perror("\nBind failed");
 		return 1;
 	}
 
+	// listening on port
 	listen(socket_desc, 3);
 
-	if (socket_client = accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c))
+	while (socket_client = accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c)) // loop runs until there is a new client
 	{
 		printf("\nClient Connected\n");
 		new_sock = malloc(1);
 		*new_sock = socket_client;
 
-		while (HandleConn(new_sock))
-			;
+		while (HandleConn(new_sock)); // runs until the client exits
+		close(socket_client);
 	}
 
 	if (socket_client < 0)
